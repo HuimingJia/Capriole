610e41fd24d39b2cd65db08c52290cce2f39628e
HuimingJia <jiahm92@gmail.com>
2017-04-05 23:01:30 -0400

    nav

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
+    @program_us = Program.where(program_type: 0)
+    @program_cn = Program.where(program_type: 1)
   end
 
   def about
