0270d56bdfabf345b476855afa1547aaf4963928
HuimingJia <jiahm92@gmail.com>
2017-04-05 20:47:58 -0400

    reshape the architecture

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,16 +1,13 @@
 class MailsController < ApplicationController
-
   def send(data=nil)
     first_name=params[:first_name]
-    second_name=params[:second_name]
+    second_name=params[:last_name]
     email=params[:email]
     phone=params[:phone]
     camp=params[:camp]
     inquire_type=params[:inquire_type]
     message=params[:message]
-
     UserMailer.contact_confirmation(first_name,second_name,email,phone,camp,inquire_type,message).deliver
-
-    
+    redirect_to contactus_path
   end
 end
